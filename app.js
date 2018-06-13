f5ff0d92bedbe006cf11340f729c2ff4f2335576
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-06-13 13:26:45 -0300

    se corrige id comentario

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const boton = document.getElementById("btn");
 
 boton.addEventListener("click", ()=> {
-    let comentarios = document.getElementById("comentario").value;
-    document.getElementById("comentario").value="";
+    let comentarios = document.getElementById("tweet").value;
+    document.getElementById("tweet").value="";
     const twiteado = document.getElementById("twiteado");
     const nuevosComentarios = document.createElement("div");
     const contenedorElemento = document.createElement("p");
@@ -10,5 +10,5 @@ boton.addEventListener("click", ()=> {
   contenedorElemento.appendChild(textNuevoComentario);
   nuevosComentarios.appendChild(contenedorElemento);
   twiteado.appendChild(nuevosComentarios);
-//No entiendo por que no me funciona, más tarde seguiré intentando
+
 })
